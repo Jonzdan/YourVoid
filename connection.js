d8d8fc5c389dc7a1de2f9c4475aa1ac565cf0025
Jonzdan <110751038+Jonzdan@users.noreply.github.com>
2022-08-12 18:33:46 -0400

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const mysql2 = require('mysql2');
 const pool = mysql2.createPool({
     connectionLimit: 100,
-    host: 'localhost',
-    user: 'root',
-    password: 'Fjj937637848!',
-    database: 'yourvoiddb',
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'ba21c6470393b0',
+    password: 'aca45a73',
+    database: 'heroku_aa6c97d55ecec6f',
 })
 
 //FASDHvnauycHHAFJSHDvlASDBJCA MYSQL MASTER PASSWORD
