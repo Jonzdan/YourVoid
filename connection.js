ccf0dd6db0e4b5500a2f3aeef97eb76843c445da
Jonzdan <110751038+Jonzdan@users.noreply.github.com>
2022-11-01 11:44:48 -0400

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const mysql2 = require('mysql2');
 const pool = mysql2.createPool({
     connectionLimit: 1000,
-    host: `${process.env.HOST_NAME}`,
-    user: `${process.env.USER_NAME}`,
-    password: `${process.env.PASS_NAME}`,
-    database: `${process.env.DATABASE}`,
+    host: process.env.HOST_NAME,
+    user: process.env.USER_NAME,
+    password: process.env.PASS_NAME,
+    database: process.env.DB_NAME,
 })
 
 module.exports = pool;
